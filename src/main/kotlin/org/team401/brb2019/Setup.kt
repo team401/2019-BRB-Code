67332b3d84485db91a4db199b61fccace9e8ed3b
Nicholas Stoll <nicholasstoll@mcps.org>
2019-09-09 20:23:21 -0400

    Added Intake State Machine

--- src/main/kotlin/org/team401/brb2019/Setup.kt
+++ src/main/kotlin/org/team401/brb2019/Setup.kt
@@ -1,8 +1,9 @@
 package org.team401.brb2019
 
 import org.snakeskin.dsl.*
+import org.team401.brb2019.subsystems.CargoSubsystem
 
 @Setup
 fun setup() {
-
+    Subsystems.add(CargoSubsystem)
 }
--- src/main/kotlin/org/team401/brb2019/subsystems/CargoSubsystem.kt
+++ src/main/kotlin/org/team401/brb2019/subsystems/CargoSubsystem.kt
@@ -1,8 +1,35 @@
 package org.team401.brb2019.subsystems
 
+import edu.wpi.first.wpilibj.Solenoid
 import org.snakeskin.dsl.*
+import org.snakeskin.event.Events
 
 object CargoSubsystem : Subsystem() {
+    private val intakeSolenoid = Solenoid(1)
 
+    enum class IntakeStates {
+        Intaking,
+        Stowed
+    }
+
+    val intakeMachine: StateMachine<IntakeStates> = stateMachine {
+        state(CargoSubsystem.IntakeStates.Intaking) {
+           entry {
+               intakeSolenoid.set(true)
+           }
+        }
+
+        state(CargoSubsystem.IntakeStates.Stowed) {
+            entry {
+                intakeSolenoid.set(false)
+            }
+        }
+    }
+
+    override fun setup() {
+        on(Events.TELEOP_ENABLED) {
+            intakeMachine.setState(CargoSubsystem.IntakeStates.Stowed)
+        }
+    }
 
 }
