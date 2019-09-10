9d55ec97ae2f88ec9e4a4ae021a920b56e02b0a2
Ayca Bulbul <aycabulbul@mcps.org>
2019-09-09 20:28:41 -0400

    Added Hatch State Machine

--- src/main/kotlin/org/team401/brb2019/Setup.kt
+++ src/main/kotlin/org/team401/brb2019/Setup.kt
@@ -1,9 +1,10 @@
 package org.team401.brb2019
 
 import org.snakeskin.dsl.*
+import org.team401.brb2019.Subsystems.HatchSubsystem
 import org.team401.brb2019.subsystems.CargoSubsystem
 
 @Setup
 fun setup() {
-    Subsystems.add(CargoSubsystem)
+    Subsystems.add(CargoSubsystem, HatchSubsystem)
 }
