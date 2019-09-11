e2e9a424941c761b4e64f48538677ffc6ed81799
Ayca Bulbul <aycabulbul@mcps.org>
2019-09-11 18:36:38 -0400

    Added Hatch State Machine 2

--- src/main/kotlin/org/team401/brb2019/Subsystems/HatchSubsystem.kt
+++ src/main/kotlin/org/team401/brb2019/Subsystems/HatchSubsystem.kt
@@ -1,7 +1,34 @@
-package org.team401.brb2019.Subsystems
+package org.team401.brb2019.subsystems
 
+import edu.wpi.first.wpilibj.Solenoid
 import org.snakeskin.dsl.*;
+import org.snakeskin.event.Events
 
 object HatchSubsystem : Subsystem() {
+    val pusherSolenoid = Solenoid(0)
 
+    enum class PusherStates {
+        In,
+        Out
+    }
+
+    val pusherMachine: StateMachine<PusherStates> = stateMachine {
+        state(PusherStates.In){
+            entry {
+                pusherSolenoid.set(false)
+            }
+        }
+
+        state(PusherStates.Out){
+            entry{
+                pusherSolenoid.set(true)
+            }
+        }
+    }
+
+    override fun setup() {
+        on(Events.TELEOP_ENABLED){
+            pusherMachine.setState(HatchSubsystem.PusherStates.In)
+        }
+    }
 }
